89e6eb8c20ab8f86cb837ae7a8fde802e5554e73
mai11223213213213 <matvijko1606@gmail.com>
2025-02-27 22:27:02 +0200

    kjhg

--- src/App.js
+++ src/App.js
@@ -1,37 +1,76 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import React, {useState} from 'react';
 import Option from './components/Option';
 import Section from './components/Section';
-
-
-
+import img1 from "./assets/img/right_1.png";
+import img2 from "./assets/img/right_2.png";
+import img3 from "./assets/img/right_3.png";
+import img4 from "./assets/img/wrong_1.png";
+import img5 from "./assets/img/wrong_2.png";
+import img6 from "./assets/img/wrong_3.png";
+import img7 from "./assets/img/wrong_4.png";
+import img8 from "./assets/img/wrong_5.png";
+import img9 from "./assets/img/wrong_6.png";
 
 const App = () => {
+  const [sections, setSections] = useState([null, null, null]);
+
+  // Масив доступних опцій (фільтрується при перетягуванні)
+  const [availableOptions, setAvailableOptions] = useState([
+    { id: 1, src: img1 },
+    { id: 2, src: img2 },
+    { id: 3, src: img3 },
+    { id: 4, src: img4 },
+    { id: 5, src: img5 },
+    { id: 6, src: img6 },
+    { id: 7, src: img7 },
+    { id: 8, src: img8 },
+    { id: 9, src: img9 },
+  ]);
+  console.log(availableOptions)
+  console.log(sections)
+
   const [draggedItem, setDraggedItem] = useState(null);
 
-  const handleDragStart = (e, id) => {
-    setDraggedItem(id);
+  // Початок перетягування
+  const handleDragStart = (e, item) => {
+    setDraggedItem(item);
   };
 
-  const handleDrop = (e) => {
-    e.preventDefault();
-    console.log('Dropped item id:', draggedItem);
-    setDraggedItem(null);
+  // Відпускання елемента у секції
+  const handleDrop = (index) => {
+    if (sections[index] === null) {
+      const updatedSections = [...sections];
+      updatedSections[index] = draggedItem;
+      setSections(updatedSections);
+
+      // Видаляємо з опцій перетягнутий елемент
+      setAvailableOptions(availableOptions.filter((option) => option.id !== draggedItem.id));
+
+      setDraggedItem(null);
+    }
   };
 
-  const handleDragOver = (e) => {
-    e.preventDefault();
+  // Повернення елемента назад в опції
+  const handleReturn = (index) => {
+    const updatedSections = [...sections];
+    const returnedItem = updatedSections[index];
+    updatedSections[index] = null;
+    setSections(updatedSections);
+
+    // Додаємо елемент назад у список доступних
+    setAvailableOptions([...availableOptions, returnedItem]);
   };
-  
 
   return (
     <div className="App">
-      <Section onDrop={handleDrop} onDragOver={handleDragOver} />
-      <Option onDragStart={handleDragStart} />
+      {/* Секції для перетягування */}
+      <Section sections={sections} onDrop={handleDrop} onReturn={handleReturn} />
+
+      {/* Опції для перетягування */}
+      <Option onDragStart={handleDragStart} options={availableOptions} />
     </div>
   );
 };
 
 export default App;
-
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
-const Section = ({ onDrop, onDragOver }) => {
+const Section = ({ sections, onDrop, onReturn }) => {
   return (
-    <div className='section'>
-      {[1, 2, 3].map((id) => (
+    <div className="section">
+      {sections.map((item, index) => (
         <div
-          key={id}
-          className='section-box'
-          onDrop={(e) => onDrop(e)}
-          onDragOver={(e) => onDragOver(e)}
-        ></div>
+          key={index}
+          className="section-box"
+          onDrop={() => onDrop(index)}
+          onDragOver={(e) => e.preventDefault()}
+        >
+          {item ? (
+            <img
+              src={item.src}
+              alt="Img"
+              className="section-img"
+              onClick={() => onReturn(index)}
+            />
+           ) : ""//(
+          //   <span className="placeholder">Drop here</span>
+          // )
+        }
+        </div>
       ))}
     </div>
   );
